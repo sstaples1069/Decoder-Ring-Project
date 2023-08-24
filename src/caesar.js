37746f36befc810634b3fee41fa40f6db4806e0a
sstaples1069 <scottstarkstaples@gmail.com>
2023-08-24 14:49:12 -0700

    polybius function

--- src/caesar.js
+++ src/caesar.js
@@ -7,7 +7,7 @@ const caesarModule = (function () {
   // you can add any code you want within this function scope
 
   function caesar(input="", shift=0, encode = true) {
-    // your solution code here
+    //initial validations
     if(shift === 0 || shift < -25 || shift > 25){
       return false
     }
@@ -26,17 +26,8 @@ const caesarModule = (function () {
       shift = -shift
     }
     let result = [];
-    //need to use index value of the letter in the alphArray that matches the letter from the msgArray
-    //if the element in msgArray = " ", need to return " "
-    //then add shift to create new index value and return that letter from alphArray
-
     //for each element in msgArray - find in alphArray and return index
     msgArray.forEach((letter)=>{
-      //accounted for spaces but how do i account for any that aren't a letter?
-     
-      //if(!alphabet.some((ltr)=>ltr===letter)){result.push(letter)}
-     
-
       if(letter === " " || letter === "," || letter === "'" || letter === "." || letter === "?" ){
         result.push(letter)
       }
@@ -45,7 +36,7 @@ const caesarModule = (function () {
           const index = alphArray.indexOf(ltr)
           let newIdx = index + shift
           //how to handle value increasing or decreasing over/under thresholds
-            //modulo= "%" if i take the result of shift and modules 25 it will return the remaining value from dividing the number by the number given (25) which is the new index
+          //modulo= "%" if i take the result of shift and modulo 26 it will return the remaining value from dividing the number by the number given (26) which is the new index
           if(newIdx > 25 ) {
             newIdx = newIdx % 26
           }
--- src/polybius.js
+++ src/polybius.js
@@ -5,9 +5,10 @@
 
 const polybiusModule = (function () {
   // you can add any code you want within this function scope
+  
   const template = [
     ["a","b","c","d","e"],
-    ["f","g","h","i/j","k"],
+    ["f","g","h","(i/j)","k"],
     ["l","m","n","o","p"],
     ["q","r","s","t","u"],
     ["v","w","x","y","z"," "]
@@ -17,10 +18,64 @@ const polybiusModule = (function () {
     // your solution code here
     //encoding
     if(encode){
-
+      //individualize characters from input into an array
+      let message = input.split("");
+      //format the message to lower case and for i/j
+      let formattedMsg = message.map((string)=>{
+        let toLowerCase = string.toLowerCase();
+        //need to combine i & j
+        if(toLowerCase === "i" || toLowerCase === "j"){
+          return "(i/j)";
+        }
+        return toLowerCase;
+      })
+      //number pairs come from template array, indeces = x/y coordinates
+      //store pairs in x/y arrays
+      let yRow = [];
+      //need to search each element of the template array (or "row", which would be the 'y' value) for the input letter
+      let xColumn = formattedMsg.map((letter)=>{
+        //loop through the template to see if row contains letter
+        for (let i = 0; i < template.length; i++){
+          //identify the row by index number as y elemnet
+          const row = template[i];
+          //search for the letter needed
+          if(row.find((ltrInRow)=>ltrInRow === letter)) {
+            //has access to idx where the condition returns wihin element which is x coordinate
+            yRow.push(i + 1);
+            //if true will return the index of which row the letter was in + 1 since idx starts at 0 as the y coordinate
+            return row.indexOf(letter) +1;
+          }
+        }
+      });
+      //need to combine the xy coordinates - can use reduce to change xColumn arr to new arr of pairs (y coord is first value of pair)
+      //space added at idx5 of template[4] (x=6,y=5) to return a space
+      codedPairs = yRow.reduce((acc, yCoord, idx)=>{
+        let pair = `${xColumn[idx]}${yCoord}`;
+        if(pair === "65"){
+          pair = " ";
+        }
+        acc.push(pair);
+        //return acc which becomes codedPairs array
+        return acc
+      },[]);
+      return codedPairs.join("");
     }
+
     //decoding
     if(!encode){
+      //can use replace method to account for spaces
+      let formattedInput = input.replace(" ", 65);
+      //needs to check if input is all pairs, a string of even length
+      if(formattedInput.length % 2 !== 0) return false;
+      //match method to create an array of each separate pair
+      let coordinates = formattedInput.match(/..?/g);
+      console.log(coordinates);
+      decodedMsg = coordinates.map((xy)=>{
+        let rowIndex = xy.split("")[1] - 1;
+        let columnIndex = xy.split("")[0] - 1;
+        return template[rowIndex][columnIndex]
+      })
+      return decodedMsg.join("");
 
     }
   }
